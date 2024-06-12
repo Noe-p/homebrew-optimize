93a709331e603eac94fffe44279395f10fd6af44
Noé <noe.philippe@bob-le-developpeur.com>
2024-06-12 11:20:43 +0200

    Version v1.0.9

--- generate-qr.rb
+++ generate-qr.rb
@@ -1,7 +1,7 @@
 class GenerateQr < Formula
   desc "Permet de générer un QR code"
   homepage "https://github.com/Noe-p/homebrew-scripts"
-  url "https://github.com/Noe-p/homebrew-scripts.git", :tag => "v1.0.8"
+  url "https://github.com/Noe-p/homebrew-scripts.git", :tag => "v1.0.9"
   license "MIT"
 
   depends_on "qrencode"
