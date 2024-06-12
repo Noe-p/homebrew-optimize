8a6c07546615701f385cbdfb9bcc0adb8c79cab9
Noé <noe.philippe@bob-le-developpeur.com>
2024-06-12 11:37:27 +0200

    Version v1.0.11

--- optimize.rb
+++ optimize.rb
@@ -1,13 +1,13 @@
 class Optimize < Formula
   desc "Optimizes and resizes images and videos"
   homepage "https://github.com/Noe-p/homebrew-scripts"
-  url "https://github.com/Noe-p/homebrew-scripts.git", :tag => "v1.0.7"
+  url "https://github.com/Noe-p/homebrew-scripts.git", :tag => "v1.0.11"
   license "MIT"
 
   depends_on "imagemagick"
 
   def install
-    bin.install "optimize.sh" => "optimize"
+    bin.install "./scripts/optimize.sh" => "optimize"
   end
 
   test do
